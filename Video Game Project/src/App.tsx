b8f35798e645fc69b1902f36611d182dd6e0e4a3
21ntphilos <ucheokoye99@gmail.com>
2023-11-19 19:29:51 +0100

    layout done

--- Video Game Project/src/App.tsx
+++ Video Game Project/src/App.tsx
@@ -1,11 +1,26 @@
 import Button from "./components/button"
 import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
 
 
   return (
     <>
+      <Grid templateAreas={{ 
+        base: `"nav" "body"`,
+        md: `"nav" "side body"`,
+        lg: `"nav nav" "side body"`
+       }}
+    >
+        <GridItem area={"nav"} bg={"gray"}>Nav</GridItem>
+        <Show above="lg">
+           <GridItem area={"side"} bg={"brown"}>Side</GridItem>
+        </Show>
+        <GridItem area={"body"} bg={"yellow"}>body</GridItem>
+      
+    </Grid>
+
       <Button/>
     </>
   )
