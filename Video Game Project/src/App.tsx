bbf602a10b91e8f9cb191c7a44f5d3394c9a6902
21ntphilos <ucheokoye99@gmail.com>
2023-11-19 19:51:58 +0100

    NavBar Init

--- Video Game Project/src/App.tsx
+++ Video Game Project/src/App.tsx
@@ -1,27 +1,30 @@
 import Button from "./components/button"
 import './App.css'
 import { Grid, GridItem, Show } from "@chakra-ui/react"
+import Navbar from "./components/navbar"
 
 function App() {
 
 
   return (
     <>
-      <Grid templateAreas={{ 
+      <Grid templateAreas={{
         base: `"nav" "body"`,
         md: `"nav" "side body"`,
         lg: `"nav nav" "side body"`
-       }}
-    >
-        <GridItem area={"nav"} bg={"gray"}>Nav</GridItem>
+      }}
+      >
+        <GridItem area={"nav"} bg={"gray"}>
+          <Navbar/>
+          </GridItem>
         <Show above="lg">
-           <GridItem area={"side"} bg={"brown"}>Side</GridItem>
+          <GridItem area={"side"} bg={"brown"}>Side</GridItem>
         </Show>
         <GridItem area={"body"} bg={"yellow"}>body</GridItem>
-      
-    </Grid>
 
-      <Button/>
+      </Grid>
+
+      <Button />
     </>
   )
 }
