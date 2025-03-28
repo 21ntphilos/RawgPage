05cfa8095cb434ec90ed31d581130c9f2bc39621
21ntphilos <ucheokoye99@gmail.com>
2025-03-28 20:51:50 +0100

    Introduced env

--- Video Game Project/src/service/apiClient.ts
+++ Video Game Project/src/service/apiClient.ts
@@ -1,9 +1,9 @@
 import axios from  'axios'
 
-
+const{ KEY } = process.env
 export default axios.create({
 	baseURL: "https://api.rawg.io/api",
 	params: {
-		key: "12a4750d3d4b4aeba3c1431df989c1a5",
+		key: KEY!,
 	},
 });
